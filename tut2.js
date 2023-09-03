c13229f7c97e30270395090e3f3e0fbd20d47334
Virendara <virendara1224@gmail.com>
2023-09-03 16:41:51 +0530

    first commit

---
+++ tut2.js
@@ -0,0 +1,20 @@
+(function (){
+ const form=document.querySelector('#message-form')
+ form.addEventListener('submit',function(e) {
+    e.preventDefault()
+
+    const message=document.querySelector('#message')
+    const feedback=document.querySelector('.feedback')
+    const messagecontent=document.querySelector('.messagecontent')
+
+    if( message.value === ''){
+
+    }
+    else {
+        
+    }
+ })
+
+
+
+})
